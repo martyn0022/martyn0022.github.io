000cc35f10b0a8e4cf2ad44708486f1873034fc1
martyn <32283437+martyn0022@users.noreply.github.com>
2021-09-19 17:42:45 +0800

    fa

--- src/decks/crossover/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    bumbleDate:       require('./bumbleDate'),
-    bumbleBFF:        require('./bumbleBFF'),
-    bumbleBizz:       require('./bumbleBizz'),
-    valentino:        require('./valentino'),
-    cann:             require('./cann'),
-}
